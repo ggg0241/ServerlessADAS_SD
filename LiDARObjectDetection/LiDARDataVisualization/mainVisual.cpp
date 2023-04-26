64be4cbde17f100e9942971bce425d571d463b02
Gabriel Gibbons <39013375+ggg0241@users.noreply.github.com>
2023-04-26 17:12:53 -0400

    Added initial c project for object detection

---
+++ LiDARObjectDetection/LiDARDataVisualization/mainVisual.cpp
@@ -0,0 +1,10 @@
+/*
+	Helper application, visualize LiDAR data, make a circular radar display
+	possibly have a popup red on object detection
+	
+	Last modified: 4/25/23
+*/
+#include "stdio.h"
+#include "stdint.h"
+#include "Windows.h"
+
