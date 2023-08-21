86a4c119b466baf424daa939702c942071a9a922
Gabriel Gibbons <39013375+ggg0241@users.noreply.github.com>
2023-08-21 12:57:09 -0400

    Create auto_brake.py
    Added the first of the controllers from the final project, the automatic braking function controller

---
+++ controllersub/auto_brake.py
@@ -0,0 +1,47 @@
+import paho.mqtt.client as mqtt
+import time
+import picar_4wd as fc
+
+# Define the MQTT broker address and port
+BROKER = "localhost"  # Replace with your MQTT broker's IP or hostname
+PORT = 1883
+
+# The callback function when a message is received from the MQTT broker
+def on_message(client, userdata, message):
+    topic = message.topic
+    payload = message.payload.decode("utf-8")
+
+    print(f"Received message: {payload} on topic: {topic}")
+
+    # Check the received message and call corresponding functions
+    if payload == "stop":
+        fc.stop()
+    #elif payload == 35:
+    #    fc.forward(35)
+    #elif payload == 25:
+    #    fc.forward(25)
+    #elif payload == 15:
+    #    fc.forward(15)
+    else:
+        fc.forward(50)
+
+# Functions to be invoked based on the received messagon 3 invoked!")
+
+def main():
+    # Create a MQTT client
+    client = mqtt.Client()
+
+    # Assign the on_message callback function
+    client.on_message = on_message
+
+    # Connect to the MQTT broker
+    client.connect(BROKER, PORT, 60)
+
+    # Subscribe to the "controller" topic
+    client.subscribe("controller")
+
+    # Start the MQTT client loop to receive messages
+    client.loop_forever()
+
+if __name__ == "__main__":
+    main()
