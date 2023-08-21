18eaa98ba5a4538bec04ab7333beb141ebdd0a47
Gabriel Gibbons <39013375+ggg0241@users.noreply.github.com>
2023-08-21 13:12:52 -0400

    Update handler.py

--- sub/handler.py
+++ sub/handler.py
@@ -3,11 +3,18 @@ import paho.mqtt.client as mqtt
 
 TOPIC = 'controller'
 PORT = 1883
-BROKER = "172.17.0.1" #mosquitto broker IP
+# third ip address found using 'hostname -I'
+BROKER = "172.17.0.1"
 
 def handle(event):
     message = event.decode()
     print("Received message: {}".format(message))
+    # parses the input to get the distance (only useful for adaptive cruise control application)
+    #test = message.split()
+    #test1 = test[3]
+    #distance = int(test1[:-1])
+
+    # right now only publishes 'stop' but can be worked on
     msg = 'stop'
     client = mqtt.Client()
 
